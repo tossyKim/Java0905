eee06d32133bc4fe02ad360a873087c37618d1fb
Tossy Kim <dydrkfk22@gmail.com>
2024-10-17 09:54:35 +0900

    업캐스팅: 범위제한 실습

---
+++ src/chp10/ex10_1/ex10_4/MainSquare.java
@@ -0,0 +1,12 @@
+package chp10.ex10_1.ex10_4;
+
+public class MainSquare{
+
+    public static void main(String[] args) {
+//        object, shape, square 객체의 필드와 메소드 접근 가능
+        Square square = new Square();
+//         shape 객체의 필드와 메소드 접근이 가능하다.(예외적으로 Square 객체의 멤버를 접근할 수도 있다.)
+        Shape shape = new Square();
+
+    }
+}
--- src/chp10/ex10_1/ex10_4/Square.java
+++ src/chp10/ex10_1/ex10_4/Square.java
@@ -2,5 +2,10 @@ package chp10.ex10_1.ex10_4;
 
 public class Square extends Shape{
 
+    int pointNum = 4;
 
+    public double area(int width) {
+        double area = Math.pow(width,2);
+        return area;
+    }
 }
